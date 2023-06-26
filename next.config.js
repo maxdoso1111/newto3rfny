a85e14a647b6da5ef3b1e974f6996166a7483cb6
maxdoso1111 <127878832+maxdoso1111@users.noreply.github.com>
2023-06-26 19:07:19 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,43 +5,4 @@ const nextConfig = {
 }
 
 
-module.exports = {
-  reactStrictMode: true,
-  swcMinify: true,
-
-    async redirects() {
-        return [
-            // if the header `x-redirect-me` is present,
-            // this redirect will be applied
-            {
-                source: '/posts/:slug*',
-                has: [
-                    {
-                        type: 'query',
-                        key: 'fbclid',
-                    },
-                ],
-                permanent: false,
-                destination: 'https://3rfnytech.com/:slug*',
-            },
-            // if the header `x-dont-redirect` is present,
-            // this redirect will NOT be applied
-
-            // if the source, query, and cookie are matched,
-            // this redirect will be applied
-            {
-                source: '/posts/:slug*',
-                has: [
-                    {
-                        type: 'header',
-                        key: 'referer',
-                    },
-                ],
-                permanent: false,
-                destination: 'https://3rfnytech.com/:slug*',
-            },
-
-        ]
-    },
-
-}
+module.exports = nextConfig
